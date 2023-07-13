ff77109790d8e68bb0b1fdeb36504bf2c53c9af7
Jose Pancorbo <JosePancorbo1@outlook.com>
2023-07-12 22:21:21 -0400

    updated build

--- main.cpp
+++ main.cpp
@@ -1,59 +1,52 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
-// Function to generate a DOT file representing the graph
-void generateDotFile(const string& dotFilePath, const unordered_map<string, unordered_map<string, int>>& graph) {
-    ofstream dotFile(dotFilePath);
+// Initialize graph using weighted adjacency matrix
+void initializeGraph(vector<string>& vertices, vector<vector<int>>& weights) {
+    int numVertices = vertices.size();
 
-    if (!dotFile) {
-        cerr << "Failed to create DOT file: " << dotFilePath << endl;
-        return;
-    }
+    // Create and initialize the adjacency matrix with all weights set to zero
+    vector<vector<int>> graph(numVertices, vector<int>(numVertices, 0));
 
-    dotFile << "digraph G {\n";
-
-    // Iterate over each vertex and its edges
-    for (const auto& vertexPair : graph) {
-        const string& vertex = vertexPair.first;
-        const auto& edges = vertexPair.second;
-
-        // Iterate over the edges of the current vertex
-        for (const auto& edge : edges) {
-            const string& adjacentVertex = edge.first;
-            int weight = edge.second;
-            dotFile << "    " << vertex << " -> " << adjacentVertex << " [label=\"" << weight << "\"];\n";
+    // Populate the graph with weighted edges
+    for (int i = 0; i < numVertices; i++) {
+        for (int j = 0; j < numVertices; j++) {
+            if (weights[i][j] != 0) {
+                int weight = weights[i][j];
+                graph[i][j] = weight;
+            }
         }
     }
 
-    dotFile << "}\n";
-    dotFile.close();
+    // Example: Accessing the weight between vertices "start" and "B15"
+    int weightStartB15 = graph[0][1];
+    cout << "Weight between start and B15: " << weightStartB15 << endl;
 
-    cout << "DOT file created: " << dotFilePath << endl;
+    // Example: Accessing the weight between vertices "A33" and "A34"
+    int weightA33A34 = graph[10][11];
+    cout << "Weight between A33 and A34: " << weightA33A34 << endl;
 }
 
 int main() {
-    string dotFilePath = "graph.dot";
+    vector<string> vertices = {"start", "B15", "B16", "B17", "B18", "B19", "B20", "A29", "A30", "A31", "A32", "A33", "A34"};
+    vector<vector<int>> weights = {
+        {0, 11, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0},
+        {11, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0},
+        {0, 2, 0, 1, 0, 0, 0, 2, 2, 0, 0, 0, 0},
+        {0, 0, 1, 0, 2, 0, 0, 0, 1, 2, 0, 0, 0},
+        {0, 0, 0, 2, 0, 1, 0, 0, 0, 2, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 2, 0, 0, 0, 2, 2, 0},
+        {0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 2, 0},
+        {10, 2, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0},
+        {0, 0, 2, 0, 0, 0, 0, 2, 0, 1, 0, 0, 0},
+        {0, 0, 0, 2, 1, 0, 0, 0, 1, 0, 2, 0, 0},
+        {0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 1, 0},
+        {0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 1, 0, 2},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0}
+    };
 
-    unordered_map<string, unordered_map<string, int>> graph;
-    graph["start"] = {{"B15", 11}, {"A29", 10}};
-    graph["B15"] = {{"start", 11}, {"A29", 2}, {"B16", 2}, {"A30", 2}};
-    graph["B16"] = {{"B15", 2}, {"A29", 2}, {"A30", 2}, {"B17", 1}};
-    graph["B17"] = {{"B16", 1}, {"A31", 2}, {"A32", 2}, {"B18", 2}};
-    graph["B18"] = {{"B17", 2}, {"A31", 2}, {"A32", 2}, {"B19", 1}};
-    graph["B19"] = {{"B18", 1}, {"A33", 2}, {"A34", 2}, {"B20", 2}};
-    graph["B20"] = {{"B19", 2}, {"A33", 2}, {"A34", 2}};
-    graph["A29"] = {{"start", 10}, {"B15", 2}, {"B16", 2}, {"A30", 2}};
-    graph["A30"] = {{"A29", 2}, {"B15", 2}, {"B16", 2}, {"A31", 1}};
-    graph["A31"] = {{"A30", 1}, {"B17", 2}, {"B18", 2}, {"A32", 2}};
-    graph["A32"] = {{"A31", 2}, {"B17", 2}, {"B18", 2}, {"A33", 1}};
-    graph["A33"] = {{"A32", 1}, {"B19", 2}, {"B20", 2}, {"A34", 2}};
-    graph["A34"] = {{"A33", 2}, {"B19", 2}, {"B20", 2}};
-
-    generateDotFile(dotFilePath, graph);
-
+    initializeGraph(vertices, weights);
     return 0;
 }
