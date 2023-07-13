715276031b32e8c51cd2efe11d6f4348e58f25a5
Jose Pancorbo <JosePancorbo1@outlook.com>
2023-07-12 21:30:40 -0400

    updated vertices

--- main.cpp
+++ main.cpp
@@ -38,3 +38,22 @@ void generateDotFile(const string& dotFilePath, const unordered_map<string, unor
 int main() {
     string dotFilePath = "graph.dot";
 
+    unordered_map<string, unordered_map<string, int>> graph;
+    graph["start"] = {{"B15", 11}, {"A29", 10}};
+    graph["B15"] = {{"start", 11}, {"A29", 2}, {"B16", 2}, {"A30", 2}};
+    graph["B16"] = {{"B15", 2}, {"A29", 2}, {"A30", 2}, {"B17", 1}};
+    graph["B17"] = {{"B16", 1}, {"A31", 2}, {"A32", 2}, {"B18", 2}};
+    graph["B18"] = {{"B17", 2}, {"A31", 2}, {"A32", 2}, {"B19", 1}};
+    graph["B19"] = {{"B18", 1}, {"A33", 2}, {"A34", 2}, {"B20", 2}};
+    graph["B20"] = {{"B19", 2}, {"A33", 2}, {"A34", 2}};
+    graph["A29"] = {{"start", 10}, {"B15", 2}, {"B16", 2}, {"A30", 2}};
+    graph["A30"] = {{"A29", 2}, {"B15", 2}, {"B16", 2}, {"A31", 1}};
+    graph["A31"] = {{"A30", 1}, {"B17", 2}, {"B18", 2}, {"A32", 2}};
+    graph["A32"] = {{"A31", 2}, {"B17", 2}, {"B18", 2}, {"A33", 1}};
+    graph["A33"] = {{"A32", 1}, {"B19", 2}, {"B20", 2}, {"A34", 2}};
+    graph["A34"] = {{"A33", 2}, {"B19", 2}, {"B20", 2}};
+
+    generateDotFile(dotFilePath, graph);
+
+    return 0;
+}
